473acf1f9774a9c571d148be8886bbe1b9295575
Ravinder Singh <tech_ravinder@leadinglogistic.com>
2022-06-03 15:46:19 +0530

    Div removed

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import { Chat } from "./modules/chat/chat";
 
 function App() {
   return (
-    <div>
+    <>
       <Routes>
         <Route path="/payment" element={<Payment/>} />
         <Route path="/chat" element={<Chat/>} />
       </Routes>
-    </div>
+    </>
   );
 }
 
